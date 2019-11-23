3c7182ca5aca3c16b3ad0254b430c6ef3a3ffaa3
yessygarcia <yesseniagarcia989@gmail.com>
2019-11-23 12:50:09 -0600

    Actualizando proyecto Alba

--- view/mostrar_bien.php
+++ view/mostrar_bien.php
@@ -35,15 +35,11 @@
                 <table id="tabla-activos" class="highlight responsive-table">
                   <thead>
                       <tr>
-                          <th>Código Interno</th>
-                          <th>Código MINED</th>
-                          <th>Código ITCA</th>
+                          <th>Códigos</th>
                           <th>Clasificación</th>
                           <th>Tipo</th>
                           <th>Descripción</th>
-                          <th>Marca</th>
-                          <th>Modelo</th>
-                          <th>Serie</th>
+                          <th>Detalles</th>
                           <th>Usuario custodio</th>
                           <th>Ubicación</th>
                           <th>Costo ($)</th>
@@ -51,25 +47,28 @@
                           <th>Observaciones</th>
                           <th class="center"><i title="Editar" class="small material-icons black-text">border_color</i></th>
                           <th class="center"><i title="Desactivar" class="small material-icons black-text">delete_forever</i></th>
+                          <th>Adicional</th>
                       </tr>
                   </thead>
                   <tbody>
                   <?php foreach($this->model->ListarBienActivos() as $r): ?>
                       <tr>
-                          <td><?php echo mb_strtoupper($r->codigointerno,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->codigomined,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->codigoitca,'UTF-8'); ?></td>
+                      <td><b>Interno:</b>&nbsp;<?php echo mb_strtoupper($r->codigointerno!=null)? $r->codigointerno: "N/A"; ?><br>
+                            <b>MINED</b>:&nbsp;<?php echo mb_strtoupper($r->codigomined!=null)? $r->codigomined: "N/A"; ?><br>
+                            <b>ITCA:</b>&nbsp;<?php echo mb_strtoupper($r->codigoitca!=null)? $r->codigoitca: "N/A"; ?></td>
                           <td><?php echo mb_strtoupper($r->idclasificacionbien,'UTF-8'); ?></td>
                           <td><?php echo mb_strtoupper($r->tipobien,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->descripcionbien,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->marca,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->modelo,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->serie,'UTF-8'); ?></td>
+                          <td><?php echo mb_strtoupper($r->descripcionbien,'UTF-8'); ?>
+                      </td>
+                      <td><b>Marca:</b>&nbsp;<?php echo mb_strtoupper($r->marca!=null)? $r->marca: "N/A"; ?><br>
+                          <b>Modelo</b>:&nbsp;<?php echo mb_strtoupper($r->modelo!=null)? $r->modelo: "N/A"; ?><br>
+                          <b>Serie:</b>&nbsp;<?php echo mb_strtoupper($r->serie!=null)? $r->serie: "N/A"; ?></td>    
                           <td><?php echo mb_strtoupper($r->idusuariocustodio,'UTF-8'); ?></td>
                           <td><?php echo mb_strtoupper($r->idubicacion,'UTF-8'); ?></td>
                           <td style="text-align:right;"><?php echo mb_strtoupper($r->costobien,'UTF-8'); ?></td>
                           <td><?php echo mb_strtoupper($r->idfuentefinanciamiento,'UTF-8'); ?></td>
-                          <td><?php echo mb_strtoupper($r->observaciones,'UTF-8'); ?></td>
+                          <td><?php echo mb_strtoupper($r->observaciones,'UTF-8'); ?>
+                      </td>
                           
                           <td class="center">
                               <!-- en la url pasamos parámetros para el controlador -->
@@ -78,8 +77,16 @@
                           </td>
                           <td class="center">
                           <a onclick="javascript:return confirm('¿Seguro/a que desea descartar este bien?');" href="?c=Bien&a=CambiarEstado&nuevo_estado=4&id=<?php echo $r->idbien; ?>" title="Desactivar Registro" ><i class="small material-icons red-text">cancel</i></a>
-                       
-                           </td>
+                          </td>
+
+                          <td class="center">
+                              <!-- en la url pasamos parámetros para el controlador -->
+                              <!--    controller, metod,id -->
+                              <a href="pdf"target="_blank" title="Imprimir en Pdf" ><i class="small material-icons blue-text">archive</i></a><br>
+                              <a href="NuevoExcel/excel.php" title="Imprimir en Excel" ><i class="small material-icons green-text">print</i></a><br>
+                              <a href="pdf"target="_blank" title="Ver detalles" ><i class="small material-icons red-text">border_color</i></a>
+                         
+                          </td>
                       </tr>
                   <?php endforeach; ?>
                   </tbody>
